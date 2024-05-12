a4a567279d560f0a914a906af650d8a998c4a4ca
GianlivioIemolo <gianlivioiemolo@gmail.com>
2024-05-12 15:23:26 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script>
+import axios from "axios";
+import { store } from "./store";
+import AppHeader from './components/AppHeader.vue';
+
+export default {
+
+  components: { 
+    AppHeader,
+  },
+
+
+  data () {
+    store,
+  }
+
+}
+
+</script>
+
+<template>
+  <AppHeader />
+</template>
+
+<style lang="scss">
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,19 @@
+<script>
+import axios from 'axios';
+import {store} from "../store.js"
+
+export default {
+    data() {
+        return {
+            store,
+        }
+    },
+}
+</script>
+
+<template>
+
+</template>
+
+<style scoped lang="scss">
+</style>
